d59cc2c1117628e6d5a9a019be1113dca65735b1
Yusong Yue <yusong.yue.@team.telstra.com>
2019-10-06 21:19:38 +1100

    Add test coverage report

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
+import { createBrowserHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from 'stores';
 import App from './App';
 
 it('renders without crashing', () => {
+  const history = createBrowserHistory();
+  const store = configureStore(history);
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App history={history} />
+    </Provider>,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
